aa994967352713b6b4dd27621b068e4214503de0
hapihomes <117711526+hapihomes@users.noreply.github.com>
2025-07-29 10:42:34 +0800

    Initial Upload

---
+++ sales_overdue_invoices_reminder/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Sales Overdue Invoices Reminder',
+    'version': '1.0',
+    'description': 'Sends email notifications every 3 days to salespersons about their overdue invoices.',
+    'summary': 'Automatically notifies salespersons of their overdue invoices',
+    'author': 'Joff Pascual',
+    'website': 'https://joffpascual.odoo.com',
+    'depends': [
+        'account', 'sales'
+    ],
+    'data': [
+        'data/cron.xml',
+    ],
+    'installable' : True,
+    'application' : False,
+    'auto_install' : False,
+        
+}
