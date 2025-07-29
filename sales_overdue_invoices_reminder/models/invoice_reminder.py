023a91302f93844c57b1f600893212d273987dc4
hapihomes <117711526+hapihomes@users.noreply.github.com>
2025-07-29 13:46:12 +0800

    Delete sales_overdue_invoices_reminder directory

--- sales_overdue_invoices_reminder/models/invoice_reminder.py
+++
@@ -1,8 +0,0 @@
-""" from odoo import models, fields, api 
-from datetime import date 
-
-class InvoiceReminder(models.Model):
-    _inherit = 'account.move'
-
-    @api.model 
-    def send_overdue_invoice_reminders """
