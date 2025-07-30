fa479cc1a09de08a61957b3fd554d00d5cc9d42e
hapihomes <117711526+hapihomes@users.noreply.github.com>
2025-07-30 13:57:02 +0800

    Update __manifest__.py

--- sales_overdue_invoices_reminder/__manifest__.py
+++ sales_overdue_invoices_reminder/__manifest__.py
@@ -1,5 +1,4 @@
-{
-    'name': 'Sales Overdue Invoices Reminder',
+{'name': 'Sales Overdue Invoices Reminder',
     'version': '1.0',
     'description': 'Sends email notifications every 3 days to salespersons about their overdue invoices.',
     'summary': 'Automatically notifies salespersons of their overdue invoices',
@@ -13,5 +12,4 @@
     ],
     'installable': True,
     'application': False,
-    'auto_install': False
-}
+    'auto_install': False}
